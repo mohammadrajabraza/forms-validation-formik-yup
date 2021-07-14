ebdbf8ed4f4ccb27c811b2c987e51432c10d4be5
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-14 12:05:08 +0500

    used getFieldProps to use implicit onChange, onBlur, value, checked props for fields

--- src/index.js
+++ src/index.js
@@ -25,33 +25,18 @@ const SignupForm = () => {
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="email">Email Address</label>
       <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
+        id="email" type="email" {...formik.getFieldProps('email')}
       />
       {formik.touched.email && formik.errors.email ? <div> {formik.errors.email} </div> : null}
       
       <label htmlFor="firstname">First Name</label>
       <input
-        id="firstname"
-        name="firstname"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstname}
+        id="firstname" type="text" {...formik.getFieldProps('firstname')}
       />
       {formik.touched.firstname && formik.errors.firstname ? <div>{formik.errors.firstname}</div> : null}
       <label htmlFor="lastname">Last Name</label>
       <input
-        id="lastname"
-        name="lastname"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastname}
+        id="lastname" type="text" {...formik.getFieldProps('lastname')}
       />
       {formik.touched.lastname && formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
       <br/>
