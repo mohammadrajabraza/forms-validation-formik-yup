0507d4a29225d20dbe9ac2858393d10052bc904e
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-14 10:57:36 +0500

    basic form UI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import { useFormik } from "formik"
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: { 
+      email: "",
+      firstname: "",
+      lastname: "" 
+    },
+    onSubmit: values => {
+      console.log(JSON.stringify(values, null, 2))
+    }
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      
+      <label htmlFor="firstname">First Name</label>
+      <input
+        id="firstname"
+        name="firstname"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstname}
+      />
+      
+      <label htmlFor="lastname">Last Name</label>
+      <input
+        id="lastname"
+        name="lastname"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastname}
+      />
+      
+      <br/>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+
+function App() {
+  return <SignupForm />
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
