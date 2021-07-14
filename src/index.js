bfebbec2f5c1196b9cea53cecec95ff1bfd2ae06
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-14 12:59:03 +0500

    replace useFormik with Formik component to reduce to code even more

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { useFormik } from "formik"
+import { Formik } from "formik"
 import * as Yup from 'yup'
 import "./index.css"
 
 const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: { 
-      email: "",
-      firstname: "",
-      lastname: "" 
-    },
-    validationSchema: Yup.object({
-      firstname:  Yup.string().required('Required').max(15, 'Must be 15 characters or less'),
+  return <Formik
+    initialValues={{ email: "", firstname: "", lastname: "" }}
+    validationSchema={Yup.object({
+      firstname: Yup.string().required('Required').max(15, 'Must be 15 characters or less'),
       lastname: Yup.string().required('Required').max(20, 'Must be 20 characters or less'),
       email: Yup.string().required('Required').email('Invalid email address')
-    }) ,
-    onSubmit: values => {
+    })}
+    onSubmit={(values => {
       console.log(JSON.stringify(values, null, 2))
-    }
-  })
+    })
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
+    }>
+    {formik => (<form onSubmit={formik.handleSubmit}>
       <label htmlFor="email">Email Address</label>
       <input
         id="email" type="email" {...formik.getFieldProps('email')}
       />
       {formik.touched.email && formik.errors.email ? <div> {formik.errors.email} </div> : null}
-      
+
       <label htmlFor="firstname">First Name</label>
       <input
         id="firstname" type="text" {...formik.getFieldProps('firstname')}
@@ -39,12 +34,13 @@ const SignupForm = () => {
         id="lastname" type="text" {...formik.getFieldProps('lastname')}
       />
       {formik.touched.lastname && formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
-      <br/>
+      <br />
       <button type="submit">Submit</button>
-    </form>
-  );
-};
+    </form>)
+    }
 
+  </Formik>
+}
 
 function App() {
   return <SignupForm />
