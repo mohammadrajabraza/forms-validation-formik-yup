4dd9c85c03e89391bbb8375cd6b99c0e603ec1e3
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-14 13:38:21 +0500

    added Field, Error, Form components from Formik
    added Field, Error, Form components from Formik to get rid of explicit props and complex error messages displaying logic

--- src/index.js
+++ src/index.js
@@ -1,44 +1,51 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Formik } from "formik"
+import { Formik, Form, Field, ErrorMessage } from "formik"
 import * as Yup from 'yup'
 import "./index.css"
 
 const SignupForm = () => {
   return <Formik
-    initialValues={{ email: "", firstname: "", lastname: "" }}
+    initialValues={{ email: "", fullname: "", streetAddress: "", city: "" }}
     validationSchema={Yup.object({
-      firstname: Yup.string().required('Required').max(15, 'Must be 15 characters or less'),
-      lastname: Yup.string().required('Required').max(20, 'Must be 20 characters or less'),
-      email: Yup.string().required('Required').email('Invalid email address')
+      fullname: Yup.string().required('Required').max(15, 'Must be 15 characters or less'),
+      email: Yup.string().required('Required').email('Invalid email address'),
+      streetAddress: Yup.string().required('Required').max(50, 'Must be 50 characters or less'),
+      city: Yup.string().required('Required')
+      
     })}
     onSubmit={(values => {
       console.log(JSON.stringify(values, null, 2))
     })
 
     }>
-    {formik => (<form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email" type="email" {...formik.getFieldProps('email')}
-      />
-      {formik.touched.email && formik.errors.email ? <div> {formik.errors.email} </div> : null}
+      <Form>
 
-      <label htmlFor="firstname">First Name</label>
-      <input
-        id="firstname" type="text" {...formik.getFieldProps('firstname')}
-      />
-      {formik.touched.firstname && formik.errors.firstname ? <div>{formik.errors.firstname}</div> : null}
-      <label htmlFor="lastname">Last Name</label>
-      <input
-        id="lastname" type="text" {...formik.getFieldProps('lastname')}
-      />
-      {formik.touched.lastname && formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
-      <br />
-      <button type="submit">Submit</button>
-    </form>)
-    }
+        <label htmlFor="fullname">First Name</label>
+        <Field type="text" name="fullname"/>
+        <ErrorMessage name="fullname"/>
+        
+        <label htmlFor="email">Email Address</label>
+        <Field type="email" name="email"/>
+        <ErrorMessage name="email"/>
 
+        <label htmlFor="streetAddress">Street Address</label>
+        <Field name="streetAddress" as="textarea"/>
+        <ErrorMessage name="streetAddress"/>
+
+        <label htmlFor="city">City</label>
+        <Field as="select" name="city">
+          <option value="">{`< select - city >`}</option>
+          <option value="Karachi">Karachi</option>
+          <option value="Lahore">Lahore</option>
+          <option value="Islamabad">Islamabad</option>
+        </Field>
+        <ErrorMessage name="city"/>
+        
+        <br />
+        
+        <button type="submit">Submit</button>
+    </Form>
   </Formik>
 }
 
