9b52a182c2382178bee098f2bc38743e99227360
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-15 11:23:20 +0500

    Styled input password and made custom input components
    - Same styling for password fields as other input fields and added shaking animation for error case
    
    - Added custom input components for getting formik fields' props with less code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Formik, Form, Field, ErrorMessage } from "formik"
+import { Formik, Form, useField } from "formik"
 import * as Yup from 'yup'
 import "./index.css"
 
+const inputErrorStyle = {
+  border: "2px solid var(--red-600)",
+  animation: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both"
+}
+
+
+const MyTextInput = ({ label, ...props}) => {
+
+  const [field, meta] = useField(props)
+  return <>
+      <label htmlFor={props.id || props.name}> {label} </label>
+      <input className="text-input" style={ meta.touched && meta.error ? inputErrorStyle : {} } {...field} {...props} />
+      { meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+  </>
+}
+
+const MyCheckBox = ({ children, ...props}) => {
+
+  const [field, meta] = useField({...props, type: 'checkbox'})
+  return <div>
+    <label>
+      <input type="checkbox" {...field} {...props} 
+        style={ meta.touched && meta.error ? inputErrorStyle : {} }/>
+      {children}
+    </label>
+    {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+  </div>
+}
+
+const MySelect = ({label, ...props}) => {
+
+  const [field, meta] = useField(props)
+
+  return <div>
+    <label htmlFor={props.id || props.name}>{label}</label>
+    <select {...props} {...field} style={ meta.touched && meta.error ? inputErrorStyle : {} }/>
+    {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+  </div>
+}
+
+const MyTextArea = ({label, ...props}) => {
+  const [field, meta] =useField(props)
+  return <div>
+    <label htmlFor={props.id || props.name}>{label}</label>
+    <textarea {...field} {...props} style={ meta.touched && meta.error ? inputErrorStyle : {} }></textarea>
+    {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+  </div>
+}
+
 const SignupForm = () => {
+
+  
+
+
   return <Formik
-    initialValues={{ email: "", fullname: "", streetAddress: "", city: "" }}
+    initialValues={{ email: "", fullname: "", streetAddress: "", password: "",
+      confirmPassword: "", city: "", acceptedTerms: false, jobType: ''}}
     validationSchema={Yup.object({
       fullname: Yup.string().required('Required').max(15, 'Must be 15 characters or less'),
       email: Yup.string().required('Required').email('Invalid email address'),
-      streetAddress: Yup.string().required('Required').max(50, 'Must be 50 characters or less'),
-      city: Yup.string().required('Required')
+      streetAddress: Yup.string().required('Required'),
+      password: Yup.string().required('Required').min(8, 'Must contain at least 8 characters')
+        .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,}$/,
+        "Must contain 1 uppercase, 1 lowercase, 1 number and 1 special case character"
+      ),
+      confirmPassword: Yup.string().required('Required')
+     .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+      acceptedTerms: Yup.boolean().required('Required')
+        .oneOf([true], 'You must accept the terms and conditions.'),
+      jobType: Yup.string().required('Required')
+        .oneOf(
+          ['designer', 'development', 'product', 'other'],
+          'Invalid Job Type'
+        )
       
     })}
     onSubmit={(values => {
@@ -19,33 +86,56 @@ const SignupForm = () => {
     })
 
     }>
+    {formik =>(
       <Form>
+        <MyTextInput
+          label="Full Name"
+          name="fullname"
+          placeholder="Ahmed khan"
+          type="text"
+          // style={formik.errors.fullname ? inputErrorStyle : {}}
+        />
 
-        <label htmlFor="fullname">First Name</label>
-        <Field type="text" name="fullname"/>
-        <ErrorMessage name="fullname"/>
+        <MyTextInput
+          name="email"
+          type="email"
+          placeholder="abc@example.com"
+          label="Email"
+        />
+
+        <MyTextInput
+          label="Password"
+          name="password"
+          type="password"
+        />
+        <MyTextInput
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+        />
+
+        <MyTextArea
+          label="Street Address"
+          name="streetAddress"
+          placeholder="123, 10-A street, upfare hill"
+        />
+
+        <MySelect label="Job Type" name="jobType">
+          <option value="">Select a job type</option>
+          <option value="designer">Designer</option>
+          <option value="development">Developer</option>
+          <option value="product">Product Manager</option>
+          <option value="other">Other</option>
+        </MySelect> 
         
-        <label htmlFor="email">Email Address</label>
-        <Field type="email" name="email"/>
-        <ErrorMessage name="email"/>
-
-        <label htmlFor="streetAddress">Street Address</label>
-        <Field name="streetAddress" as="textarea"/>
-        <ErrorMessage name="streetAddress"/>
-
-        <label htmlFor="city">City</label>
-        <Field as="select" name="city">
-          <option value="">{`< select - city >`}</option>
-          <option value="Karachi">Karachi</option>
-          <option value="Lahore">Lahore</option>
-          <option value="Islamabad">Islamabad</option>
-        </Field>
-        <ErrorMessage name="city"/>
-        
+        <MyCheckBox name="acceptedTerms">
+             I accept the terms and conditions
+        </MyCheckBox>
         <br />
-        
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={!formik.dirty || !formik.isValid}>Submit</button>
+        <button type="reset" onClick={formik.handleReset}>Reset Form</button>
     </Form>
+    )}
   </Formik>
 }
 
